fa468fc7aa1ae85713b524ef9a39afc4b78b2eb8
buttaakhil <159509138+buttaakhil@users.noreply.github.com>
2024-06-15 13:56:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const navOption=document.querySelectorAll(".nav-single-option");
+for(let obj of navOption){
+    obj.addEventListener("mouseover",()=>{
+        obj.style.backgroundColor="#A22FFF";
+    });
+    obj.addEventListener("mouseout",()=>{
+        obj.style.backgroundColor="";
+    })
+}
+
+const phoneNavOptions=document.querySelectorAll(".phone-nav-options");
+for(let obj of phoneNavOptions){
+    obj.addEventListener("mouseover",()=>{
+        obj.style.backgroundColor="#A22FFF";
+    });
+    obj.addEventListener("mouseout",()=>{
+        obj.style.backgroundColor="";
+    })
+}
+
+const closeHam=document.querySelector(".close-hambargar");
+const navListPhone=document.querySelector(".outer-after-click-nav");
+const openHam=document.querySelector(".show-hambargar");
+const TopnavPhone=document.querySelector(".phone-nav-outer");
+
+closeHam.addEventListener("click",()=>{
+    navListPhone.style.display="none";
+    TopnavPhone.style.display="flex";
+});
+
+openHam.addEventListener("click",()=>{
+    navListPhone.style.display="flex";
+    TopnavPhone.style.display="none";
+});
+
+const visitSite=document.querySelectorAll(".visit-site");
+const gotoGitHub=document.querySelectorAll(".go-to-github");
+
+for(let obj of visitSite){
+    obj.addEventListener("mouseover",()=>{
+        obj.style.backgroundColor="#A22FFF";
+        obj.style.color="white";
+    });
+    obj.addEventListener("mouseout",()=>{
+        obj.style.backgroundColor="";
+        obj.style.color="#A22FFF";
+    });
+}
+
+
+for(let obj of gotoGitHub){
+    obj.addEventListener("mouseover",()=>{
+        obj.style.backgroundColor="#A22FFF";
+        obj.style.color="white";
+    });
+    obj.addEventListener("mouseout",()=>{
+        obj.style.backgroundColor="";
+        obj.style.color="#C072FF";
+    });
+}
+
+
+const emailForm=document.querySelector(".email-form");
+const inputForm=document.querySelector(".idea-input");
+emailForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    inputForm.value="";
+})
